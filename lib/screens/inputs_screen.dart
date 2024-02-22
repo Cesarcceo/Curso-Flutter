3acbd25e693c5d896e7deb4cdd42ac9c9e5b9083
Sara <saraitzelgarc@gmail.com>
2024-02-22 10:24:51 -0600

    Upgrading dependencies (inputs_screen.dart): :arrow_up: se agrego un boton y un mapa para verificar los herrores del formulario

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -7,47 +7,70 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'nombre': 'Cesario',
+      'id': '548',
+      'email': 'mincesario@gmail',
+      'contrasena': '8987',
+      'role': 'Adim',
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs y Forms'),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Column(
-            children: [
-              CustomInputField(
-                labelText: 'Tu nombre, hijo de mama coco',
-                helperText: 'solo nombres',
-                hintText: 'Maama coco te pregunta tu nombre',
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              CustomInputField(
-                labelText: 'Tu id, hijo de mama coco',
-                helperText: 'solo numeros',
-                hintText: 'Maama coco te pregunta tu id',
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              CustomInputField(
-                labelText: 'Tu correo, hijo de mama coco',
-                helperText: 'solo correos',
-                hintText: 'Maama coco te pregunta tu correo',
-                keyboardType: TextInputType.emailAddress,
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              CustomInputField(
-                labelText: 'Tu correo, hijo de mama coco',
-                helperText: 'solo correos',
-                hintText: 'Maama coco te pregunta tu correo',
-                obcureText: true,
-              ),
-            ],
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                const CustomInputField(
+                  labelText: 'Tu nombre, hijo de mama coco',
+                  helperText: 'solo nombres',
+                  hintText: 'Maama coco te pregunta tu nombre',
+                ),
+                const SizedBox(height: 25),
+                const CustomInputField(
+                  labelText: 'Tu id, hijo de mama coco',
+                  helperText: 'solo numeros',
+                  hintText: 'Maama coco te pregunta tu id',
+                ),
+                const SizedBox(height: 25),
+                const CustomInputField(
+                  labelText: 'Tu correo, hijo de mama coco',
+                  helperText: 'solo correos',
+                  hintText: 'Maama coco te pregunta tu correo',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 25),
+                const CustomInputField(
+                  labelText: 'Tu contrasena, hijo de mama coco',
+                  hintText: 'Maama coco te pregunta tu contrasena',
+                  obcureText: true,
+                ),
+                const SizedBox(height: 25),
+                ElevatedButton(
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus(FocusNode());
+                    if (myFormKey.currentState!.validate()) {
+                      print('formulario no valido');
+                      return;
+                    }
+                    print(formValues);
+                  },
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(
+                      child: Text('Guardar'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
